d5f35e1ebe9071fb53f1a683da025638cc712535
Jose Daniel Montoya Molina <66889936+panchuel@users.noreply.github.com>
2022-02-20 13:33:54 -0500

    Primer punto
    a

--- Sandbox.cs
+++ Sandbox.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace TestProject1
 {
@@ -6,8 +8,88 @@ namespace TestProject1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.WriteLine("Here you can write console prints to test your implementation outside the testing environment");
+            Stack<int> entrada = new Stack<int>();
+            entrada.Push(14);
+            entrada.Push(8);
+            entrada.Push(7);
+            entrada.Push(13);
+
+            /*int[] stackToArray = entrada.ToArray();
+            Stack<int> copyEntrada = new Stack<int>();
+
+            for (int i = stackToArray.Length - 1; i >= 0; i--)
+            {
+                copyEntrada.Push(stackToArray[i]);
+            }
+
+            Stack<int> salida = new Stack<int>();
+            List<int> salidas = new List<int>();
+            List<int> toSort = new List<int>();
+
+            while (copyEntrada.Count > 0)
+            {
+                int actual = copyEntrada.Pop();
+
+                salidas.Add(actual);
+
+                int valorMayor = actual;
+
+                for (int i = 0; i < salidas.Count; i++)
+                {
+                    if(salidas[i] > valorMayor) valorMayor = salidas[i];
+                }
+
+                if(valorMayor == actual) toSort.Add(-1);
+                else toSort.Add(valorMayor);
+            }
+
+            for (int i = toSort.Count-1; i >= 0; i--)
+			{
+                salida.Push(toSort[i]);
+			}
+
+            while(salida.Count > 0)
+            {
+                Console.WriteLine(salida.Pop());
+            }*/
+            
+            int[] copiaEntrada = new int[entrada.Count];
+            entrada.CopyTo(copiaEntrada, 0);
+            Stack<int> salida = new Stack<int>();
+            List<int> salidas = new List<int>();
+            List<int> toSort = new List<int>();
+ 
+            int valorMayor = 0, valorActual = 0;
+            
+            while(copiaEntrada.Length > 0)
+            {
+                for (int i = 0; i < copiaEntrada.Length; i++)
+                {
+
+                    valorActual = copiaEntrada[i];
+                    valorMayor = valorActual;
+                    for (int j = i; j < copiaEntrada.Length; j++)
+                    {
+                        if (valorActual < copiaEntrada[j])
+                        {
+                            valorMayor = copiaEntrada[j];
+                        }
+                    }
+                    if (valorMayor == valorActual)
+                    {
+                        salida.Push(-1);
+                    }
+                    else
+                    {
+                        salida.Push(valorMayor);
+                    }
+                }
+            }
+            
+            while(salida.Count > 0)
+            {
+                Console.WriteLine(salida.Pop());
+            }
         }
     }
 }
--- TestMethods.cs
+++ TestMethods.cs
@@ -15,7 +15,42 @@ namespace TestProject1
 
         internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
         {
-            Stack<int> result = null;
+            int[] stackToArray = new int[sourceStack.Count];
+            sourceStack.CopyTo(stackToArray, 0);                                                        //Convierte el source en un arreglo para poder hacerle una copia profunda
+            Stack<int> copySource = new Stack<int>();                                                   //Pila donde se guarda la copia de source
+
+            
+            for (int i = stackToArray.Length - 1; i >= 0; i--)                                          //Organiza de nuevo la pila, que se desordenó al convertirla en un arreglo
+            {
+                copySource.Push(stackToArray[i]);
+            }
+
+            Stack<int> result = new Stack<int>();                                                       
+            List<int> salidas = new List<int>();                                                        //Donde se guardan los datos que salen de la copia de Source
+            List<int> toSort = new List<int>();                                                         //Se guardan los resultados para ordenarlos al final y que respete el orden del
+            int max = copySource.Count;
+
+            for (int i = 0; i < max; i++)
+            {
+                int actual = copySource.Pop();
+
+                salidas.Add(actual);                                                                    //El dato que salga de la copia del source se añade a la lista
+
+                int valorMayor = actual;                                                                //Se toma el valor actual como el valor mayor
+
+                for (int j = 0; j < salidas.Count; j++)
+                {
+                    if (salidas[j] > valorMayor) valorMayor = salidas[j];                               //Se revisan los demas elementos que han salido de la copia del source para ver si hay uno mayor que el guardado anteriormente
+                }
+
+                if (valorMayor == actual) toSort.Add(-1);
+                else toSort.Add(valorMayor);
+            }
+
+            for (int i = toSort.Count - 1; i >= 0; i--)                                                 //Se recorre de forma inversa para ordenarlos y dejarlos equivalentes a la copia de la pila
+            {
+                result.Push(toSort[i]);
+            }
 
             return result;
         }
